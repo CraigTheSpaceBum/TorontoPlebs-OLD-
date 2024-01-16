1d9c7e8b0b22356f33d2e4dd8ebb4bed13dacd95
Craig <142868948+CraigTheSpaceBum@users.noreply.github.com>
2024-01-15 20:29:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,37 @@ async function loadContent(page) {
 
     switch (page) {
         case 'home':
-            // Load the home.html for the home page
             const homeResponse = await fetch('home.html');
             content = await homeResponse.text();
             break;
         case 'news':
-            // Load the news.html for the NEWS page
             const newsResponse = await fetch('news.html');
             content = await newsResponse.text();
             break;
+        case 'shop':
+            const shopResponse = await fetch('shop.html');
+            content = await shopResponse.text();
+            break;
+        case 'videos':
+            const videosResponse = await fetch('videos.html');
+            content = await videosResponse.text();
+            break;
         case 'interviews':
-            // Load the interviews.html for the INTERVIEWS page
             const interviewsResponse = await fetch('interviews.html');
             content = await interviewsResponse.text();
             break;
-        case 'events':
-            // Load the events.html for the EVENTS page
-            const eventsResponse = await fetch('events.html');
-            content = await eventsResponse.text();
+        case 'whatisnostr':
+            const whatisnostrResponse = await fetch('whatisnostr.html');
+            content = await whatisnostrResponse.text();
             break;
-        case 'about':
-            // Load the about.html for the ABOUT page
-            const aboutResponse = await fetch('about.html');
-            content = await aboutResponse.text();
+        case 'nostraddress':
+            const nostraddressResponse = await fetch('nostraddress.html');
+            content = await nostraddressResponse.text();
             break;
-        case 'nostr':
-            // Redirect to an external link for NOSTR
-            window.location.href = 'https://njump.me/@torontoplebs.ca';
-            return;
+        case 'settings':
+            const settingsResponse = await fetch('settings.html');
+            content = await settingsResponse.text();
+            break;
         default:
             content = 'Page not found!';
     }
@@ -38,7 +41,26 @@ async function loadContent(page) {
     document.getElementById('main-content').innerHTML = content;
 }
 
-// Load 'home.html' initially
 document.addEventListener('DOMContentLoaded', function () {
     loadContent('home');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownBtn = document.querySelector('.dropbtn');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownBtn.addEventListener('click', function () {
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.addEventListener('click', function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            const dropdowns = document.querySelectorAll('.dropdown-content');
+            dropdowns.forEach(function (dropdown) {
+                if (dropdown.classList.contains('show')) {
+                    dropdown.classList.remove('show');
+                }
+            });
+        }
+    });
 });
